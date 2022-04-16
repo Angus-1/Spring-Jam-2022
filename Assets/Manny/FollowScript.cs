d08dd85f66601fe3444b38b65dfa319bf02ba278
emmerdroid <54683249+Manny-op@users.noreply.github.com>
2022-04-16 12:31:37 -0700

    Created a follow script

---
+++ Assets/Manny/FollowScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowScript : MonoBehaviour
+{
+    private GameObject player;
+    public float moveSpeed;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+    }
+    private void FixedUpdate()
+    {
+        if(Vector2.Distance(player.transform.position, transform.position) >= 2.0f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, moveSpeed * Time.deltaTime);
+
+        }
+    }
+}
