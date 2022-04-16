7b0bbcf19d20e88b930ee9cb453ce32e943d150a
emmerdroid <54683249+Manny-op@users.noreply.github.com>
2022-04-16 11:04:29 -0700

    Initial commit

---
+++ Assets/Manny/Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody rb;
+    BoxCollider bc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        bc = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Movement()
+    {
+
+    }
+}
