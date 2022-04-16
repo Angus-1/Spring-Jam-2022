8c32cf0b70aeb235dae065edafcae8b25aa2f91e
santoyox714 <santoyox714@gmail.com>
2022-04-16 13:10:47 -0700

    Duplication script

---
+++ Assets/duplication/Duplicaton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Duplicaton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject prefab;
+    public GameObject player;
+
+    public float offset;
+    void Start()
+    {
+        
+        for (var i = 0; i < 2; i++)
+        {
+            offset = Random.Range(-1.1f, 1.1f);
+            Instantiate(prefab, new Vector3(i * player.transform.position.x+ offset, player.transform.position.y+ offset, player.transform.position.z + offset), Quaternion.identity);
+        }
+    }
+
+
+
+      
+    void Update()
+    {
+    
+       
+    }
+}
