3e1231706b94db55a0fed0d7ec366b0f77a75d00
emmerdroid <54683249+Manny-op@users.noreply.github.com>
2022-04-17 22:06:06 -0700

    made to export to WebGL, adjusted the scripts for web winning, may do vicotry stages later on

---
+++ Assets/TextScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextScript : MonoBehaviour
+{
+    public Text MyText;
+    public int cloneNum;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MyText.text = "Number of Followers: ";
+        cloneNum = GameObject.Find("Player").GetComponent<Movement>().numOfClones;
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        MyText.text = "Number of Followers: ";
+        cloneNum = GameObject.Find("Player").GetComponent<Movement>().numOfClones;
+    }
+}
